a8859b7a7245b7c68cbd260e3325476d20d23bb0
Tymon <tymon.szczepanski@bsr.edu.pl>
2023-03-04 19:09:31 +0100

    w.i.p. upgrade 4/03/2023

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,15 +12,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.rollButton.setOnClickListener{generate()}
+        binding.addDiceButton.setOnClickListener{addDice()}
     }
     private fun generate(){
         val wallsNum = binding.wallsNum.text.toString().toInt()
         val diceNum = binding.diceNum.text.toString().toInt()
         val random = List(diceNum){ Random.nextInt(1,wallsNum+1)}
         val randomToString = random.toString()
-        binding.textView.text = randomToString
+        binding.rollResult.text = randomToString
     }
-
-
-
+    private fun addDice(){
+        val wallsAmt = binding.wallsNum.text.toString()
+        val diceAmt = binding.diceNum.text.toString()
+        val diceNotation: String = (diceAmt + "D" + wallsAmt)
+        binding.diceList.text = diceNotation
+    }
 }
